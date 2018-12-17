e7c2d6f9c52139dc0ef728efc3e6a5a483db4655
integration <integration>
2018-12-17 13:51:10 +0100

    Correction in case of spaces in filenames

--- eol/checkEOL.sh
+++ eol/checkEOL.sh
@@ -2,10 +2,13 @@
 result=0
 
 for var in "$@"; do
-    find ./ -type f -name "$var" -print0 | xargs -0 eol_if
-    if [ $? != 0 ]; then
-        result=1
-    fi
+   files=$(find ./ -type f -name "$var" -print)
+    for file in "${files[@]}"; do
+        eol_if "$file"
+        if [ $? != 0 ]; then
+            result=1
+        fi
+    done
 done
 
 exit ${result}
--- eol/eol_if.sh
+++ eol/eol_if.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 result=0;
-
-for file in $@; do
-    if [[ -n $(dos2unix --info=c "$file") ]] ; then
-        echo "the file $file have DOS EOL"
-        result=1;
-    fi
-done
+if [[ -n $(dos2unix --info=c "$file") ]] ; then
+    echo "the file $file have DOS EOL"
+    result=1;
+fi
 
 exit ${result}
